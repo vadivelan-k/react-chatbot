db2d6f5d20c509a347ca58a5c2e116f4fecef86c
Fabien Lim <fabienlimzk@gmail.com>
2021-03-18 16:23:48 +0800

    case details, supporting doc, summary static info

--- src/Message.js
+++ src/Message.js
@@ -179,7 +179,204 @@ const Message = ({ message, index, handleSendRequest }) => {
             </div>
           );
         } else if (element.stringValue === 'case_details') {
-          actionItem = 'Case details';
+          actionItem = (
+            <div>
+              <table className={classes.BotMessage}>
+                <tr>
+                  <td>Basic Salary (Monthly): </td>
+                  <td>$2000</td>
+                </tr>
+                <tr>
+                  <td>No. of Working Days (Weekly): </td>
+                  <td>5 Days</td>
+                </tr>
+                <tr>
+                  <td>Claim Period: </td>
+                  <td>10/11/2020 - 10/12/2020</td>
+                </tr>
+                <tr>
+                  <td>No. of Actual Working Days: </td>
+                  <td>20 Days</td>
+                </tr>
+                <tr>
+                  <td>Total Pay Received </td>
+                  <td>$1000</td>
+                </tr>
+              </table>
+              <ActionButton
+                actionText={'Edit'}
+                handleSendRequest={handleSendRequest}
+              />
+              <ActionButton
+                actionText={'Submit'}
+                handleSendRequest={handleSendRequest}
+              />
+            </div>
+          );
+        } else if (element.stringValue === 'doc_upload') {
+          actionItem = (
+            <div>
+              <table className={classes.BotMessage}>
+                <tr>
+                  <td>payslip.pdf</td>
+                </tr>
+                <tr>
+                  <td>cpf.pdf</td>
+                </tr>
+                <ActionButton
+                  actionText={'Submit'}
+                  handleSendRequest={handleSendRequest}
+                />
+              </table>
+            </div>
+          );
+        } else if (element.stringValue === 'review_claim') {
+          actionItem = (
+            <div>
+              <div>
+                <h6>Personal Details</h6>
+                <table className={classes.BotMessage}>
+                  <tr>
+                    <td>Name (as per NRIC/FIN): </td>
+                    <td>TAN YUE LIANG ALEXANDER</td>
+                  </tr>
+                  <tr>
+                    <td>NRIC or FIN: </td>
+                    <td>S0000121F</td>
+                  </tr>
+                  <tr>
+                    <td>Nationality: </td>
+                    <td>Singaporean</td>
+                  </tr>
+                  <tr>
+                    <td>Date of Birth: </td>
+                    <td>12/12/1977</td>
+                  </tr>
+                  <tr>
+                    <td>Country of Birth: </td>
+                    <td>Singapore</td>
+                  </tr>
+                  <tr>
+                    <td>Pass Status: </td>
+                    <td>Active</td>
+                  </tr>
+                  <tr>
+                    <td>Pass Expiry: </td>
+                    <td>10/10/2025</td>
+                  </tr>
+                  <tr>
+                    <td>Mailing Address: </td>
+                    <td>Tampines St 92, BLK 844 #10-123 S243929</td>
+                  </tr>
+                  <tr>
+                    <td>Billing Address: </td>
+                    <td>Tampines St 92, BLK 844 #10-123 S243929</td>
+                  </tr>
+                  <tr>
+                    <td>Mobile Number: </td>
+                    <td>9642 2314</td>
+                  </tr>
+                  <tr>
+                    <td>Home Number: </td>
+                    <td>6782 1312</td>
+                  </tr>
+                  <tr>
+                    <td>Email Address: </td>
+                    <td>alexandertan@gmail.com</td>
+                  </tr>
+                  <tr>
+                    <td>Highest Education Level: </td>
+                    <td>Bachelor’s Degree</td>
+                  </tr>
+                </table>
+              </div>
+              <br />
+              <div>
+                <h6>Employment Details</h6>
+                <table className={classes.BotMessage}>
+                  <tr>
+                    <td>Occupation: </td>
+                    <td>Finance Manager</td>
+                  </tr>
+                  <tr>
+                    <td>Employment Type: </td>
+                    <td>Full Time</td>
+                  </tr>
+                  <tr>
+                    <td>Name of Employer: </td>
+                    <td>ABC Pte. Ltd</td>
+                  </tr>
+                  <tr>
+                    <td>Company UEN: </td>
+                    <td>00000000EN</td>
+                  </tr>
+                  <tr>
+                    <td>Employment Sector: </td>
+                    <td>Financial Intermediaries</td>
+                  </tr>
+                </table>
+              </div>
+              <br />
+              <div>
+                <h6>Case Details</h6>
+                <table className={classes.BotMessage}>
+                  <tr>
+                    <td>Basic Salary (Monthly): </td>
+                    <td>$2000</td>
+                  </tr>
+                  <tr>
+                    <td>No. of Working Days (Weekly): </td>
+                    <td>5 Days</td>
+                  </tr>
+                  <tr>
+                    <td>Claim Period: </td>
+                    <td>10/11/2020 - 10/12/2020</td>
+                  </tr>
+                  <tr>
+                    <td>No. of Actual Working Days: </td>
+                    <td>20 Days</td>
+                  </tr>
+                  <tr>
+                    <td>Total Pay Received: </td>
+                    <td>$1000</td>
+                  </tr>
+                </table>
+              </div>
+              <br />
+              <div>
+                <h6>Salary Calculator</h6>
+                <p className={classes.SalaryCalculator}>
+                  Here are your calculated details
+                </p>
+                <table className={classes.BotMessage}>
+                  <tr>
+                    <td>Claim Period: </td>
+                    <td>10/11/2020 - 10/12/2020</td>
+                  </tr>
+                  <tr>
+                    <td>No. of Required Working Days: </td>
+                    <td>5 Days</td>
+                  </tr>
+                  <tr>
+                    <td>No. of Actual Working Days: </td>
+                    <td>20 Days</td>
+                  </tr>
+                  <tr>
+                    <td>Pay for Incompleted Work: </td>
+                    <td>$1000</td>
+                  </tr>
+                  <tr>
+                    <td>Your Claimable Amount: </td>
+                    <td>$1000</td>
+                  </tr>
+                </table>
+              </div>
+              <ActionButton
+                actionText={'Submit'}
+                handleSendRequest={handleSendRequest}
+              />
+            </div>
+          );
         }
 
         return actionItem;
