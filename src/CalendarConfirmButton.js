d0a42b4ebffdc5d26d671a743dae7d884a0d0f89
Fabien Lim <fabienlimzk@gmail.com>
2021-03-18 17:35:31 +0800

    disable the confirm button in calendar

--- src/CalendarConfirmButton.js
+++ src/CalendarConfirmButton.js
@@ -3,12 +3,16 @@ import { Button } from 'react-bootstrap';
 import classes from './CalendarConfirmButton.module.css';
 
 const CalendarConfirmButton = ({ actionText, handleSendRequest }) => {
+  const [disabled, setDisabled] = useState(false);
   return (
     <Button
       className={classes.confirmBtn}
       variant='primary'
+      disabled={disabled}
       onClick={() => {
+        setDisabled(true);
         handleSendRequest({ actionText: actionText });
+        console.log(actionText);
       }}
     >
       Confirm
