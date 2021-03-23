fa0b6e3c1e24a9e8322bc8aa964a5506a31f204e
Vadi K <Vadivelan.Kuppusamy@cognizant.com>
2021-03-23 10:44:39 +0800

    Review claim is displaying values from backend
    - add ReviewClaimComponent.js to render details from backend
    - update Message.js to render review claim component

--- src/Message.js
+++ src/Message.js
@@ -8,6 +8,7 @@ import moment from 'moment';
 import 'react-responsive-modal/styles.css';
 import UserInfo from "./UserInfo";
 import CaseDetailComponent from "./CaseDetailComponent";
+import ReviewClaimComponent from "./ReviewClaimComponent";
 
 const Message = ({ message, index, handleSendRequest }) => {
   const [startDate, setStartDate] = useState(new Date());
@@ -101,10 +102,6 @@ const Message = ({ message, index, handleSendRequest }) => {
                 </tbody>
               </table>
               <ActionButton
-                actionText={'Edit'}
-                handleSendRequest={handleSendRequest}
-              />
-              <ActionButton
                 actionText={'Confirm'}
                 handleSendRequest={handleSendRequest}
               />
@@ -117,14 +114,12 @@ const Message = ({ message, index, handleSendRequest }) => {
             <div>
               <table className={classes.BotMessage}>
                 <tbody>
-                  <tbody>
-                    <tr>
-                      <td>payslip.pdf</td>
-                    </tr>
-                    <tr>
-                      <td>cpf.pdf</td>
-                    </tr>
-                  </tbody>
+                  <tr>
+                    <td>payslip.pdf</td>
+                  </tr>
+                  <tr>
+                    <td>cpf.pdf</td>
+                  </tr>
                 </tbody>
               </table>
               <ActionButton
@@ -134,163 +129,10 @@ const Message = ({ message, index, handleSendRequest }) => {
             </div>
           );
         } else if (element.stringValue === 'review_claim') {
-          actionItem = (
-            <div>
-              <div>
-                <h6>Personal Details</h6>
-                <table className={classes.BotMessage}>
-                  <tbody>
-                    <tr>
-                      <td>
-                        Name <br />
-                        (as per NRIC/FIN):{' '}
-                      </td>
-                      <td>TAN YUE LIANG ALEXANDER</td>
-                    </tr>
-                    <tr>
-                      <td>NRIC or FIN: </td>
-                      <td>S0000121F</td>
-                    </tr>
-                    <tr>
-                      <td>Nationality: </td>
-                      <td>Singaporean</td>
-                    </tr>
-                    <tr>
-                      <td>Date of Birth: </td>
-                      <td>12/12/1977</td>
-                    </tr>
-                    <tr>
-                      <td>Country of Birth: </td>
-                      <td>Singapore</td>
-                    </tr>
-                    <tr>
-                      <td>Pass Status: </td>
-                      <td>Active</td>
-                    </tr>
-                    <tr>
-                      <td>Pass Expiry: </td>
-                      <td>10/10/2025</td>
-                    </tr>
-                    <tr>
-                      <td>Mailing Address: </td>
-                      <td>Tampines St 92, BLK 844 #10-123 S243929</td>
-                    </tr>
-                    <tr>
-                      <td>Billing Address: </td>
-                      <td>Tampines St 92, BLK 844 #10-123 S243929</td>
-                    </tr>
-                    <tr>
-                      <td>Mobile Number: </td>
-                      <td>9642 2314</td>
-                    </tr>
-                    <tr>
-                      <td>Home Number: </td>
-                      <td>6782 1312</td>
-                    </tr>
-                    <tr>
-                      <td>Email Address: </td>
-                      <td>alexandertan@gmail.com</td>
-                    </tr>
-                    <tr>
-                      <td>Highest Education Level: </td>
-                      <td>Bachelor’s Degree</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <br />
-              <div>
-                <h6>Employment Details</h6>
-                <table className={classes.BotMessage}>
-                  <tbody>
-                    <tr>
-                      <td>Occupation: </td>
-                      <td>Finance Manager</td>
-                    </tr>
-                    <tr>
-                      <td>Employment Type: </td>
-                      <td>Full Time</td>
-                    </tr>
-                    <tr>
-                      <td>Name of Employer: </td>
-                      <td>ABC Pte. Ltd</td>
-                    </tr>
-                    <tr>
-                      <td>Company UEN: </td>
-                      <td>00000000EN</td>
-                    </tr>
-                    <tr>
-                      <td>Employment Sector: </td>
-                      <td>Financial Intermediaries</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <br />
-              <div>
-                <h6>Case Details</h6>
-                <table className={classes.BotMessage}>
-                  <tbody>
-                    <tr>
-                      <td>Basic Salary (Monthly): </td>
-                      <td>$2000</td>
-                    </tr>
-                    <tr>
-                      <td>No. of Working Days (Weekly): </td>
-                      <td>5 Days</td>
-                    </tr>
-                    <tr>
-                      <td>Claim Period: </td>
-                      <td>10/11/2020 - 10/12/2020</td>
-                    </tr>
-                    <tr>
-                      <td>No. of Actual Working Days: </td>
-                      <td>20 Days</td>
-                    </tr>
-                    <tr>
-                      <td>Total Pay Received: </td>
-                      <td>$1000</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <br />
-              <div>
-                <h6>Salary Calculator</h6>
-                <p className={classes.SalaryCalculator}>
-                  Here are your calculated details
-                </p>
-                <table className={classes.BotMessage}>
-                  <tbody>
-                    <tr>
-                      <td>Claim Period: </td>
-                      <td>10/11/2020 - 10/12/2020</td>
-                    </tr>
-                    <tr>
-                      <td>No. of Required Working Days: </td>
-                      <td>5 Days</td>
-                    </tr>
-                    <tr>
-                      <td>No. of Actual Working Days: </td>
-                      <td>20 Days</td>
-                    </tr>
-                    <tr>
-                      <td>Pay for Incompleted Work: </td>
-                      <td>$1000</td>
-                    </tr>
-                    <tr>
-                      <td>Your Claimable Amount: </td>
-                      <td>$1000</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <ActionButton
-                actionText={'Submit'}
-                handleSendRequest={handleSendRequest}
-              />
-            </div>
-          );
+          actionItem = <ReviewClaimComponent
+            handleSendRequest={handleSendRequest}
+            message={payloadMessage}
+          />
         }
 
         return actionItem;
---
+++ src/ReviewClaimComponent.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import classes from './Message.module.css';
+import ActionButton from './ActionButton';
+
+const ReviewClaimComponent = ({ handleSendRequest, message }) => {
+  const fields = message.payload.fields.form.structValue.fields;
+  // console.log(payload.fields.form.structValue.fields);
+
+  const fetchValueFromField = ({ fields, key }) => {
+    return fields[key].stringValue;
+  };
+
+  console.log(fetchValueFromField({ fields: fields, key: 'bsal' }));
+
+  return (
+    <div>
+      <div>
+        <h6>Personal Details</h6>
+        <table className={classes.BotMessage}>
+          <tbody>
+          <tr>
+            <td>
+              Name <br />
+              (as per NRIC/FIN):{' '}
+            </td>
+            <td>TAN YUE LIANG ALEXANDER</td>
+          </tr>
+          <tr>
+            <td>NRIC or FIN: </td>
+            <td>S0000121F</td>
+          </tr>
+          <tr>
+            <td>Nationality: </td>
+            <td>Singaporean</td>
+          </tr>
+          <tr>
+            <td>Date of Birth: </td>
+            <td>12/12/1977</td>
+          </tr>
+          <tr>
+            <td>Country of Birth: </td>
+            <td>Singapore</td>
+          </tr>
+          <tr>
+            <td>Pass Status: </td>
+            <td>Active</td>
+          </tr>
+          <tr>
+            <td>Pass Expiry: </td>
+            <td>10/10/2025</td>
+          </tr>
+          <tr>
+            <td>Mailing Address: </td>
+            <td>Tampines St 92, BLK 844 #10-123 S243929</td>
+          </tr>
+          <tr>
+            <td>Billing Address: </td>
+            <td>Tampines St 92, BLK 844 #10-123 S243929</td>
+          </tr>
+          <tr>
+            <td>Mobile Number: </td>
+            <td>9642 2314</td>
+          </tr>
+          <tr>
+            <td>Home Number: </td>
+            <td>6782 1312</td>
+          </tr>
+          <tr>
+            <td>Email Address: </td>
+            <td>alexandertan@gmail.com</td>
+          </tr>
+          <tr>
+            <td>Highest Education Level: </td>
+            <td>Bachelor’s Degree</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <div>
+        <h6>Employment Details</h6>
+        <table className={classes.BotMessage}>
+          <tbody>
+          <tr>
+            <td>Occupation: </td>
+            <td>Finance Manager</td>
+          </tr>
+          <tr>
+            <td>Employment Type: </td>
+            <td>Full Time</td>
+          </tr>
+          <tr>
+            <td>Name of Employer: </td>
+            <td>ABC Pte. Ltd</td>
+          </tr>
+          <tr>
+            <td>Company UEN: </td>
+            <td>00000000EN</td>
+          </tr>
+          <tr>
+            <td>Employment Sector: </td>
+            <td>Financial Intermediaries</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <div>
+        <h6>Case Details</h6>
+        <table className={classes.BotMessage}>
+          <tbody>
+          <tr>
+            <td>Basic Salary (Monthly): </td>
+            <td>S$ {fetchValueFromField({ fields: fields, key: 'bsal' })}</td>
+          </tr>
+          <tr>
+            <td>No. of Working Days (Weekly): </td>
+            <td>{fetchValueFromField({ fields: fields, key: 'wdays' })} Days</td>
+          </tr>
+          <tr>
+            <td>Claim Period: </td>
+            <td>
+              {fetchValueFromField({ fields: fields, key: 'cpstart' })} - {fetchValueFromField({ fields: fields, key: 'cpend' })}
+            </td>
+          </tr>
+          <tr>
+            <td>No. of Actual Working Days: </td>
+            <td>{fetchValueFromField({ fields: fields, key: 'awdays' })} Days</td>
+          </tr>
+          <tr>
+            <td>Total Pay Received: </td>
+            <td>S$ {fetchValueFromField({ fields: fields, key: 'rpay' })}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <div>
+        <h6>Salary Calculator</h6>
+        <p className={classes.SalaryCalculator}>
+          Here are your calculated details
+        </p>
+        <table className={classes.BotMessage}>
+          <tbody>
+          <tr>
+            <td>Claim Period: </td>
+            <td>
+              {fetchValueFromField({ fields: fields, key: 'cpstart' })} - {fetchValueFromField({ fields: fields, key: 'cpend' })}
+            </td>
+          </tr>
+          <tr>
+            <td>No. of Required Working Days: </td>
+            <td>{fetchValueFromField({ fields: fields, key: 'wdays' })} Days</td>
+          </tr>
+          <tr>
+            <td>No. of Actual Working Days: </td>
+            <td>{fetchValueFromField({ fields: fields, key: 'awdays' })} Days</td>
+          </tr>
+          <tr>
+            <td>Pay for Incompleted Work: </td>
+            <td>S$ {fetchValueFromField({ fields: fields, key: 'incompay' })}</td>
+          </tr>
+          <tr>
+            <td>Your Claimable Amount: </td>
+            <td>S$ {fetchValueFromField({ fields: fields, key: 'incompay' })}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <ActionButton
+        actionText={'Submit'}
+        handleSendRequest={handleSendRequest}
+      />
+    </div>
+  );
+};
+
+export default ReviewClaimComponent;
